5994e21d851b2e05fd62aa222b63201645376d17
Elias Myronidis <elias.myronidis@hotmail.com>
2016-10-10 12:20:03 +0300

    • Create WatchFaceService

---
+++ wearable/src/main/java/com/example/android/sunshine/app/WatchFaceService.java
@@ -0,0 +1,8 @@
+package com.example.android.sunshine.app;
+
+import android.support.wearable.watchface.CanvasWatchFaceService;
+
+public class WatchFaceService extends CanvasWatchFaceService {
+
+
+}
